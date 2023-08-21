63b421fd21722e657e898988f756a9eac89b6c39
aoinohair <aoi.no.hair@gmail.com>
2023-08-21 17:54:42 +0900

    seeder

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,6 @@ use App\Http\Controllers\ImageController;
 use App\Models\Image;
 
 
-
-
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     // ユーザーがログインしているか確認
     if (Auth::check()) {
