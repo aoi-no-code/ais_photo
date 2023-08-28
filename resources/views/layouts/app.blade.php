8fda8a5aaaae608631d0c202001f497b5f64384a
aoinohair <aoi.no.hair@gmail.com>
2023-08-28 23:00:53 +0900

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <link rel="icon" href="{{ asset('favicon.icon') }}" type="image/x-icon"/>
